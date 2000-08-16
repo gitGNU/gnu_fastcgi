f21fbd2780bce8bdeaa203a32e34c5b8a6adf205
Peter Simons <simons@cryp.to>
2000-08-16 12:17:32 +0000

    Added FCGIRequest::write(const void*, size_t).

--- request.cc
+++ request.cc
@@ -10,8 +10,8 @@
 #include "fastcgi.hpp"
 
 FCGIRequest::FCGIRequest(FCGIProtocolDriver& driver_, u_int16_t id_, u_int16_t role_, bool kc)
-	: role(role_), keep_connection(kc), have_all_params(false), aborted(false),
-	  driver(driver_), id(id_)
+	: id(id_), role(role_), keep_connection(kc), have_all_params(false),
+          aborted(false), driver(driver_)
     {
     }
 
@@ -27,7 +27,12 @@ void FCGIRequest::read(string& buf)
 
 void FCGIRequest::write(const string& buf)
     {
-    if (buf.size() > 0xffff)
+    write(buf.data(), buf.size());
+    }
+
+void FCGIRequest::write(const void* buf, size_t count)
+    {
+    if (count > 0xffff)
 	throw out_of_range("Can't send messages of that size.");
 
     // Construct message.
@@ -37,12 +42,12 @@ void FCGIRequest::write(const string& buf)
 	1,
 	FCGIProtocolDriver::TYPE_STDOUT,
 	id >> 8, id & 0xff,
-	buf.size() >> 8, buf.size() & 0xff,
+	count >> 8, count & 0xff,
 	0,
 	0
 	};
     driver.output_cb(&h, sizeof(h));
-    driver.output_cb(buf.data(), buf.size());
+    driver.output_cb(buf, count);
     }
 
 void FCGIRequest::end_request(u_int32_t appStatus, u_int8_t protStatus)
