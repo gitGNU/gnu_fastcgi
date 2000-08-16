2c288f003e8689bee5d7849658266cf15cdf720e
Peter Simons <simons@cryp.to>
2000-08-16 12:18:10 +0000

    Added lots of additional output to the test program.

--- test.cc
+++ test.cc
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include <strstream>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -61,27 +62,62 @@ try {
 	// Read input from socket and put it into the protocol driver
 	// for processing.
 
-	sleep(1);
-	char buf[4*1024];
-	ssize_t rc = read(socket, buf, sizeof(buf));
-	if (rc < 0)
-	    throw runtime_error("read() failed.");
-	driver.process_input(buf, rc);
-	FCGIRequest* req = driver.get_request();
-	if (req->keep_connection)
-	    throw runtime_error("Test program can't handle KEEP_CONNECTION.");
-	if (req->role != FCGIProtocolDriver::ROLE_RESPONDER)
-	    throw runtime_error("Test program can't handle any role but RESPONDER.");
+	FCGIRequest* req = 0;
+	do
+	    {
+	    char buf[4*1024];
+	    ssize_t rc = read(socket, buf, sizeof(buf));
+	    if (rc < 0)
+		throw runtime_error("read() failed.");
+	    driver.process_input(buf, rc);
+	    if (req == 0)
+		req = driver.get_request();
+	    }
+	while (req == 0 || req->have_all_params == false);
 
-	req->write("Content-type: text/html\r\n" \
-		   "\r\n"
-		   "<title>FastCGI echo</title>"
-		   "<h1>FastCGI echo</h1>\n");
+	// Make sure we don't get a keep-connection request.
+
+	if (req->keep_connection)
+	    {
+	    cerr << "Test program can't handle KEEP_CONNECTION." << endl;
+	    req->end_request(1, FCGIProtocolDriver::CANT_MPX_CONN);
+	    continue;
+	    }
+
+	// Make sure we are a responder.
+
+	if (req->role != FCGIProtocolDriver::ROLE_RESPONDER)
+	    {
+	    cerr << "Test program can't handle any role but RESPONDER." << endl;
+	    req->end_request(1, FCGIProtocolDriver::UNKNOWN_ROLE);
+	    continue;
+	    }
+
+	// Handle request.
+
+	cerr << "Starting to handle request." << endl;
+	ostrstream os;
+	os << "Content-type: text/html\r\n"
+	   << "\r\n"
+	   << "<title>FastCGI Test Program</title>" << endl
+	   << "<h1 align=center>FastCGI Test Program</h1>" << endl
+	   << "<h3>FastCGI Status</h3>" << endl
+	   << "Process id = " << getpid() << "<br>" << endl
+	   << "Request id = " << req->id << endl
+	   << "<h3>Request Environment</h3>" << endl;
+
+	for (map<string,string>::const_iterator i = req->params.begin(); i != req->params.end(); ++i)
+	    os << i->first << "&nbsp;=&nbsp;" << i->second << "<br>" << endl;
+
+	req->write(os.str(), os.pcount());
+	os.freeze(0);
 	req->end_request(0, FCGIProtocolDriver::REQUEST_COMPLETE);
+	cerr << "Request handled successfully." << endl;
 	}
 
     // done
 
+    cerr << "FastCGI test program terminating." << endl;
     return 0;
     }
 catch(const exception &e)
